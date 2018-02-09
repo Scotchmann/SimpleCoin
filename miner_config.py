81994405cc95e06d50ce32a8a5734ed1e417e917
Andrew Kovtunenko <andrew.kovtunenko@gmail.com>
2018-02-09 11:34:17 +0600

    Добавлены таймауты для запросов к нодам и изменен вывод запроса синхронизации

--- miner_config.py
+++ miner_config.py
@@ -5,8 +5,8 @@ more details.
 # Write your generated adress here. All coins mined will go to this address
 MINER_ADDRESS = "q3nf394hjg-andrew1-miner-address-34nf3i4nflkn3oi"
 
-MINER_IP = "10.10.10.100"
-#MINER_IP = "10.10.10.81"
+#MINER_IP = "10.10.10.100"
+MINER_IP = "10.10.10.81"
 #MINER_IP = "192.168.0.101"
 
 MINER_PORT = 5000
@@ -20,6 +20,6 @@ MINER_NODE_URL = "http://"+MINER_IP+":"+str(MINER_PORT)
 # Store the url data of every other node in the network
 # so that we can communicate with them
 #PEER_NODES = ["http://10.10.10.100:5000"]
-#PEER_NODES = ["http://10.10.10.81:5001"]
+PEER_NODES = ["http://10.10.10.81:5001"]
 #PEER_NODES = ["http://192.168.0.101:5001", "http://192.168.0.101:5002"]
-PEER_NODES = []
+#PEER_NODES = []
